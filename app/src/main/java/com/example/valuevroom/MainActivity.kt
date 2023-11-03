71078b8f171b4d4381f38b000e7352bbe94b665f
frmad <frmad21@student.sdu.dk>
2023-11-03 16:28:38 +0100

    Added vectors and fragments for the navigation bar

--- app/src/main/java/com/example/valuevroom/MainActivity.kt
+++ app/src/main/java/com/example/valuevroom/MainActivity.kt
@@ -1,5 +1,3 @@
-package com.example.valuevroom
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,14 +8,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.valuevroom.HomeFragment
+import com.example.valuevroom.R
+import com.example.valuevroom.SettingsFragment
 import com.example.valuevroom.ui.theme.ValueVroomTheme
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() /*ComponentActivity()*/ {
+
+    lateinit var bottomNav: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ValueVroomTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -26,7 +34,29 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        loadFragment(HomeFragment())
+        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.settings -> {
+                    loadFragment(SettingsFragment())
+                    true
+                }
+                else -> false
+            }
+        }
     }
+}
+
+private fun loadFragment(fragment: Fragment) {
+    val transaction = supportFragmentManager.beginTransaction()
+    transaction.replace(R.id.containerBottomNav, fragment)
+    transaction.commit()
 }
 
 @Composable
