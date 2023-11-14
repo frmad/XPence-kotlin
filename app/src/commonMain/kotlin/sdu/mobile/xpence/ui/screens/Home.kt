7fbd7b7efb354d60f2949286035c24578ac5ac55
frmad <frmad21@student.sdu.dk>
2023-11-14 22:56:33 +0100

    Began working on the Home layout
    Created a box for the title

--- app/src/commonMain/kotlin/sdu/mobile/xpence/ui/screens/Home.kt
+++ app/src/commonMain/kotlin/sdu/mobile/xpence/ui/screens/Home.kt
@@ -1,9 +1,21 @@
 package sdu.mobile.xpence.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import sdu.mobile.xpence.ui.theme.Purple80
 import sdu.mobile.xpence.ui.utils.QueryState
 import sdu.mobile.xpence.ui.utils.getGroups
 import sdu.mobile.xpence.ui.utils.usingAPI
@@ -13,7 +25,9 @@ fun Home() {
     val result by usingAPI { client ->
         getGroups(client)
     }
-
+    Column {
+        Title()
+    }
     when (val res = result) {
         is QueryState.Success -> {
             Column {
@@ -25,5 +39,33 @@ fun Home() {
 
         is QueryState.Error -> Text(text = res.message)
         is QueryState.Loading -> Text(text = "Loading")
+    }
+}
+
+@Composable
+fun Title() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()  // Fills the width of the screen
+                .height(80.dp)    // Adjust the height as needed
+                .background(Purple80, shape = RectangleShape)
+        )
+        Text(
+            text = "XPense",
+            modifier = Modifier
+                .padding(
+                    start = 5.dp,
+                    top = 30.dp,
+                    end = 30.dp,
+                    bottom = 5.dp
+                )
+                .align(Alignment.TopCenter),
+            color = Color.Black,
+        )
     }
 }
