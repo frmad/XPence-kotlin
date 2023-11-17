3ca40a6ddf3c84326036c0fbf2aaa2762eb7d39c
Oliver Kirkegaard Svendsen <37912255+Nolle10@users.noreply.github.com>
2023-11-17 12:40:59 +0100

    Add Card (#6)
    * Add Card
    * Add Text Styling
    * Rename Card to GroupCard

---
+++ app/src/commonMain/kotlin/sdu/mobile/xpence/ui/components/GroupCard.kt
@@ -0,0 +1,68 @@
+package sdu.mobile.xpence.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun GroupCard(groupTitle: String, amount: Int, currency: String) {
+    ElevatedCard(
+        shape = RoundedCornerShape(20.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier
+            .padding(
+                start = 16.dp,
+                top = 30.dp,
+                end = 16.dp,
+                bottom = 45.dp
+            )
+            .fillMaxWidth()
+    ) {
+        Text(
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            text = "$groupTitle",
+            modifier = Modifier
+                .padding(
+                start = 5.dp,
+                top = 5.dp,
+                end = 5.dp,
+                bottom = 5.dp
+                ),
+            textAlign = TextAlign.Center,
+        )
+        Row {
+            Text(
+                text = "$amount",
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+                modifier = Modifier
+                    .padding(5.dp),
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                text = "$currency.",
+                modifier = Modifier
+                    .padding(3.dp),
+                textAlign = TextAlign.Center,
+            )
+
+        }
+
+    }
+}
