6cddd1813054dc6bbcb21ed94f7385c31179821a
Mathias Gredal <matg@universal-robots.com>
2023-11-17 14:20:10 +0100

    Add back groups card

--- app/src/commonMain/kotlin/sdu/mobile/xpence/ui/tabs/GroupTab.kt
+++ app/src/commonMain/kotlin/sdu/mobile/xpence/ui/tabs/GroupTab.kt
@@ -3,16 +3,17 @@ package sdu.mobile.xpence.ui.tabs
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
-import cafe.adriel.voyager.navigator.LocalNavigator
-import cafe.adriel.voyager.navigator.currentOrThrow
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
-import sdu.mobile.xpence.ui.screens.GroupDetail
+import sdu.mobile.xpence.ui.components.GroupCard
+import sdu.mobile.xpence.ui.utils.QueryState
+import sdu.mobile.xpence.ui.utils.getGroups
+import sdu.mobile.xpence.ui.utils.usingAPI
 
 object GroupTab : Tab {
     override val options: TabOptions
@@ -31,15 +32,22 @@ object GroupTab : Tab {
 
     @Composable
     override fun Content() {
-        val navigator = LocalNavigator.currentOrThrow
-
-        Column {
-            Text(text = "Groups")
-            Button(
-                onClick = { navigator.parent?.push(GroupDetail(1)) }
-            ) {
-                Text(text = "Detail")
+        val result by usingAPI { client ->
+            getGroups(client)
+        }
+        
+        when (val res = result) {
+            is QueryState.Success -> {
+                Column {
+                    res.data.forEach { group ->
+                        GroupCard(group.name, 195, "kr")
+                    }
+                }
             }
+
+            is QueryState.Error -> Text(text = res.message)
+            is QueryState.Loading -> Text(text = "Loading")
+            else -> {}
         }
     }
 }
