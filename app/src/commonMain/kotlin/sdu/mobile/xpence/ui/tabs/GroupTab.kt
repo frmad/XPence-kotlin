220ccac87bedfcad082b63da8b84587fe034dd56
frmad <frmad21@student.sdu.dk>
2023-11-25 01:37:27 +0100

    update create group

--- app/src/commonMain/kotlin/sdu/mobile/xpence/ui/tabs/GroupTab.kt
+++ app/src/commonMain/kotlin/sdu/mobile/xpence/ui/tabs/GroupTab.kt
@@ -1,13 +1,23 @@
 package sdu.mobile.xpence.ui.tabs
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
+import sdu.mobile.xpence.ui.components.GroupCard
 import sdu.mobile.xpence.ui.components.createGroup.createGroup
+import sdu.mobile.xpence.ui.utils.QueryState
+import sdu.mobile.xpence.ui.utils.getGroups
+import sdu.mobile.xpence.ui.utils.usingAPI
 
 object GroupTab : Tab {
     override val options: TabOptions
@@ -26,26 +36,28 @@ object GroupTab : Tab {
 
     @Composable
     override fun Content() {
-        /*val result by usingAPI { client ->
+        val result by usingAPI { client ->
             getGroups(client)
         }
-        
-        when (val res = result) {
-            is QueryState.Success -> {
-                Column {
-                    res.data.forEach { group ->
-                        GroupCard(group.name, 195, "kr")
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+        ){
+            createGroup()
+
+            when (val res = result) {
+                is QueryState.Success -> {
+                    Column {
+                        res.data.forEach { group ->
+                            GroupCard(group.name, 195, "kr")
+                        }
                     }
                 }
+
+                is QueryState.Error -> Text(text = res.message)
+                is QueryState.Loading -> Text(text = "Loading")
+                else -> {}
             }
-
-            is QueryState.Error -> Text(text = res.message)
-            is QueryState.Loading -> Text(text = "Loading")
-            else -> {}
-        }*/
-
-        createGroup()
-
-
+        }
     }
 }
