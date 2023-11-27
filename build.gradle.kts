52e7c4517cdbe4237365555bf0a968913a77fea2
Mads W P <mads456dk@gmail.com>
2023-11-27 12:49:36 +0100

    Fixed requested changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,19 +7,4 @@ plugins {
     alias(libs.plugins.android.application).apply(false)
     alias(libs.plugins.libres).apply(false)
     alias(libs.plugins.buildConfig).apply(false)
-    kotlin("jvm") version "1.9.20"
-}
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-}
-repositories {
-    mavenCentral()
-}
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
 }
