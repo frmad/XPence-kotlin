d7f4274c76ef396346bd81dcc4abdf4cbe942625
Mathias Gredal <matg@universal-robots.com>
2023-11-27 13:31:11 +0100

    Expire token

--- app/src/jsMain/kotlin/sdu/mobile/xpence/Platform.js.kt
+++ app/src/jsMain/kotlin/sdu/mobile/xpence/Platform.js.kt
@@ -3,6 +3,7 @@ package sdu.mobile.xpence
 import io.ktor.client.*
 import io.ktor.client.engine.js.*
 import kotlinx.browser.window
+import kotlin.js.Date
 
 actual fun httpClient(config: HttpClientConfig<*>.() -> Unit) = HttpClient(Js, config)
 actual fun Double.formatDecimal(maxFractionDigits: Int): String = this.asDynamic().toFixed(maxFractionDigits) as String
@@ -10,4 +11,8 @@ actual fun Double.formatDecimal(maxFractionDigits: Int): String = this.asDynamic
 actual fun isIphoneWeb(): Boolean {
     val ua = window.navigator.userAgent
     return ua.contains("iPhone") || ua.contains("iPad")
+}
+
+actual fun getUnixTime(): Long {
+    return Date.now().toLong()
 }
